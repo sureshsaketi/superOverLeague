5c3457b8dffb3c0f592d0dae7481be2c513f6eee
sureshsaketi <sureshsaketi430@gmail.com>
2023-04-28 13:36:25 +0000

    main

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  //   Write your code here.
+  <div>
+    <h1 className="heading">Super Over League</h1>
+    <img
+      className="image"
+      src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+    />
+    <img
+      className="image"
+      src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+    />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
